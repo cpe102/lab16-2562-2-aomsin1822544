209c006debf17354f4ef581da49dcfe30abeb234
patcharapon manuspong <59276850+aomsin1822544@users.noreply.github.com>
2020-03-03 17:10:42 +0700

    1.2

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,7 +1,17 @@
+  
 #include<iostream>
 using namespace std;
 
 //Write the definition of myString() here
+char myString(char *m,int k){
+   char *l = new char;
+      for(int i = 0;i < k;i++){
+   	      l[i] = 'A'+i;
+   	      cout << l[i];
+   	   
+
+}
+}
 
 int main(){
 	char *p;
